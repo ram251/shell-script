6bf906c802a92011ca3b3859cdd9066c898ca3e4
root <root@ip-172-31-12-73.ap-south-1.compute.internal>
2025-01-31 11:42:55 +0000

    added conversation

--- echo.sh
+++ echo.sh
@@ -1,1 +1,7 @@
 echo "HI, I am Ramireddy"
+
+
+echo "Ramesh: Hi Suresh Good morning"
+echo "Suresh: Goodmorning Ramesh"
+echo "Ramesh: How are you Suresh"
+echo "Suresh: I am good, How are you Ramesh"
