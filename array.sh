2a5bc892e7d4f71de10934f9a5deef20364d668c
root <root@ip-172-31-12-73.ap-south-1.compute.internal>
2025-01-31 13:24:47 +0000

    arrya script

--- array.sh
+++ array.sh
@@ -3,5 +3,8 @@
 # this is called array, instead of single value it can hold multiple values.
 PERSONS=("Ramesh" "Suresh" "Chandu")
 
+# inside array e=we always refer values with index and it starts from 0
 echo "first name is ${PERSONS[0]}"
+
+echo "All persons are ${PERSONS[@]}"
 
