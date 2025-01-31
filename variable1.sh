2c83f89b88b11a6abd89a6bedb04913ee81106ab
root <root@ip-172-31-12-73.ap-south-1.compute.internal>
2025-01-31 12:20:01 +0000

    variable when execute command

---
+++ variable1.sh
@@ -0,0 +1,9 @@
+echo "HI, I am Ramireddy"
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hi $PERSON2 Good morning"
+echo "$PERSON2: Goodmorning $PERSON1"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: I am good, How are you $PERSON1"
